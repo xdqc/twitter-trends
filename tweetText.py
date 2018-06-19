751d81d5bd4729313cf32235d43c64a6d01f6724
xdqc <qichen.d@outlook.com>
2018-06-20 03:55:37 +1200

    make tweets text unigram models

--- tweetText.py
+++ tweetText.py
@@ -3,11 +3,12 @@ from collections import Counter
 
 directory = './collected_tweets/' if len(sys.argv)<2 else sys.argv[1]
 
-sentences = []
-numTweet = 0
-numTweetHashtag = 0
 
 for file in os.listdir(directory):
+    sentences = []
+    numTweet = 0
+    numTweetHashtag = 0
+
     if len(file) - len(file.replace('-','')) == 1:
         with open(directory+file, 'r') as f:
             tweets = [t for t in f.readlines() if t]
@@ -24,30 +25,44 @@ for file in os.listdir(directory):
                     text = ' '.join(hashtags) + ' ' + text[0].lower() + text[1:]
                     sentences.append(text)
 
-print('total tweets:',numTweet, '; tweets with hashtag:', numTweetHashtag, numTweetHashtag/numTweet)
-print('total sentences:', len(sentences), '; unique sentences:', len(set(sentences)))
+        print(file,'total tweets:',numTweet, '; tweets with hashtag:', numTweetHashtag, numTweetHashtag/numTweet)
+        print(file,'total sentences:', len(sentences), '; unique sentences:', len(set(sentences)))
 
-freq = Counter(sentences)
-freq = sorted(freq.items(), key=lambda x: (x[0],len(x[0])), reverse=False)
-print('sorted')
+        freq = Counter(sentences)
+        freq = sorted(freq.items(), key=lambda x: (x[0],len(x[0])), reverse=False)
+        print('sorted')
 
-sentences = [x[0] for x in freq]
+        sentences = [x[0] for x in freq]
 
-# remove similar text
-for i in range(len(sentences)-1):
-    diffstr = ''.join(difflib.ndiff(sentences[i], sentences[i+1]))
-    pluses = len(re.findall(r'[+]', diffstr))
-    minues = len(re.findall(r'[-]', diffstr))
-    if minues/len(sentences[i]) < 0.15:
-        if  minues/len(sentences[i]) > 0.1:
-            print('+',pluses,'\t-', minues, '\tl', len(sentences[i]))
-            print(sentences[i])
-            print(sentences[i+1])
-        sentences[i] = ''
+        # remove similar text
+        for i in range(len(sentences)-1):
+            diffstr = ''.join(difflib.ndiff(sentences[i], sentences[i+1]))
+            pluses = len(re.findall(r'[+]', diffstr))
+            minues = len(re.findall(r'[-]', diffstr))
+            if minues/len(sentences[i]) < 0.15:
+                if  minues/len(sentences[i]) > 0.14:
+                    print('+',pluses,'\t-', minues, '\tl', len(sentences[i]))
+                    print(sentences[i])
+                    print(sentences[i+1])
+                sentences[i] = ''
 
-outfile = 'tweetText.csv'
+        outfile = str(file).split('.')[0] + '-model.csv'
 
-with open(outfile, 'a') as w:
-    for item in sentences:
-        if item:
-            w.write(item+'\n')
+        sentences = [x for x in sentences if x]
+        print(file, 'distinct unique sentences:',len(sentences))
+        words = []
+
+        # count all words in tweet text
+        for text in sentences:
+            words.extend(w.strip('\'').lower() for w in re.findall(r'[\da-zA-Z\']+', text) if w.replace('\'',''))
+
+        totalnumberofwords = len(words)
+        print(file, 'total number of words:', totalnumberofwords)
+        freq = Counter(words)
+        print(file, 'unique words', len(freq))
+        freq = sorted(freq.items(), key=operator.itemgetter(1), reverse=True)
+
+        with open(outfile, 'a') as w:
+            for item in freq:
+                w.write(item[0]+','+str(item[1]/totalnumberofwords)+'\n')
+
