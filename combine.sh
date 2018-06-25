041c9f771ebe74bd72c54e02e079a7c1d3180ca0
xdqc <qichen.d@outlook.com>
2018-06-25 19:19:48 +1200

    trend-20180624

--- combine.sh
+++ combine.sh
@@ -11,11 +11,11 @@ for file in ${directory}/*; do
     prev=`echo $file | sed 's/.*-\([0-9]\{8\}\)-.*/\1/g'`
     echo $prev
     if [ "$prev" -lt "$today" ];then
-        comb=`echo $file | sed 's/\(-[0-9]\{8\}\).+/\1/g'`
+        comb=`echo $file | sed 's/\(-[0-9]\{8\}\).*/\1/g'`
         cat $comb* >> $comb.json
         rm -f $comb-*
     fi
 done
 
-python tweetText.py
-python trendWords.py
+python tweet_text.py
+python trend_words.py
