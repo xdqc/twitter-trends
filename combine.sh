37e8b9d270363d232ba363ce5b87e33d573cac87
Samuel Ding <samuel.ding.nz@gmail.com>
2025-05-23 13:01:15 +0800

    Update combine

--- combine.sh
+++ combine.sh
@@ -25,14 +25,14 @@ combine() {
     git add ./word-cloud/trend-${yesterday}.png
     git add ./readme.md
     git commit -m trend-${yesterday}
-    git push
+    #git push
 
     mv -f ${directory}/tweets-${yesterday}.json ${directory}/archive/tweets-${yesterday}.json
 }
 
 # do combo task once on 1:00~1:59am each day
 while true; do
-    if [ $(date +%H) -eq 23 ]; then
+    if [ $(date +%H) -eq 1 ]; then
         combine
     fi
     sleep 3598
